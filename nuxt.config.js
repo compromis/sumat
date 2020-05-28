cdf0b2236a567ca5392705dc7315c20b9ee38be4
Guillem <guillem@disedit.com>
2020-05-28 13:18:08 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,86 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: "Suma't a Compromís",
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { property: 'og:image', content: '/thumbnail.png' }
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://compromis.net/sub/fonts/compromis.css'
+      },
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'https://compromis.net/sub/icons/favicon.ico'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '32x32',
+        href: 'https://compromis.net/sub/icons/favicon32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: 'https://compromis.net/sub/icons/favicon192.png'
+      }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    './sass/style.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    'bootstrap-vue/nuxt',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <compromis-logo />
+  </div>
+</template>
+
+<script>
+import CompromisLogo from '~/components/CompromisLogo.vue'
+
+export default {
+  components: {
+    CompromisLogo
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
