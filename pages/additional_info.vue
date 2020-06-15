d57540e46cadf8826e6a53bee7b440cfe4341e10
Carles Ibáñez Golfe <carles2612@gmail.com>
2020-06-15 14:10:00 +0200

    Add more info and public employee blocks to additional info page

--- pages/additional_info.vue
+++ pages/additional_info.vue
@@ -1,12 +1,100 @@
 <template>
-  <div>
+  <div class="container">
     <form-steps :current-step="3" />
     <form :class="{ 'dimmed': submitting }" @submit.prevent="submit">
-      <input-field
-        v-model="form.u_studies"
-        name="u_studies"
-        label="Estudis"
-      />
+      <form-section
+        id="mes-info"
+        title="Vols afegir més informació?"
+      >
+        <field-group>
+          <input-select
+            v-model="form.u_labor_code"
+            name="u_labor_code"
+            label="Professió"
+            class="c-span-1 corner-top-left"
+            :options="laborCodes"
+            inline
+          />
+          <input-field
+            v-model="form.u_labor"
+            name="u_labor"
+            label="Descripció..."
+            class="c-span-3 corner-top-right"
+            maxlength="60"
+            :invalid="'u_labor' in errors"
+            :invalid-message="errors['u_labor']"
+          />
+          <input-field
+            v-model="form.u_studies"
+            name="u_studies"
+            label="Estudis"
+            class="c-span-4"
+            maxlength="70"
+            :invalid="'u_studies' in errors"
+            :invalid-message="errors['u_studies']"
+          />
+          <input-field
+            v-model="form.u_union"
+            name="u_union"
+            label="Sindicat"
+            maxlength="45"
+            class="c-span-2 corner-bottom-left"
+            :invalid="'u_union' in errors"
+            :invalid-message="errors['u_union']"
+          />
+          <input-field
+            v-model="form.u_movements"
+            name="u_movements"
+            label="Moviments culturals"
+            maxlength="45"
+            class="c-span-2 corner-bottom-right"
+            :invalid="'u_movements' in errors"
+            :invalid-message="errors['u_movements']"
+          />
+        </field-group>
+      </form-section>
+      <form-section
+        id="funcionari"
+        title="Ets Funcionari?"
+      >
+        <input-radio-group
+          v-model="isPublicEmployee"
+          name="isPublicEmployee"
+          block
+          :options="[
+            { value: 1, text: 'Si' },
+            { value: 0, text: 'No' }
+          ]"
+        />
+        <transition name="fade">
+          <field-group v-if="isPublicEmployee">
+            <input-select
+              v-model="form.u_public_employee"
+              name="u_public_employee"
+              label="Administració"
+              class="c-span-2 corner-bottom-right"
+              :options="publicEmployeeOptions"
+              inline
+            />
+            <input-select
+              v-model="form.u_public_employee_cat"
+              name="u_public_employee_cat"
+              label="Categoria"
+              :options="publicEmployeeType"
+              class="c-span-1 corner-bottom-right"
+              inline
+            />
+            <input-select
+              v-model="form.u_public_employee_type"
+              name="u_public_employee_type"
+              :options="publicEmployeeOptions"
+              label="Tipus"
+              class="c-span-1 corner-bottom-right"
+              inline
+            />
+          </field-group>
+        </transition>
+      </form-section>
       <submit-button :submitting="submitting">
         Adjunta informació
       </submit-button>
@@ -19,14 +107,22 @@
 
 <script>
 import InputField from '~/components/ui/InputField'
+import InputSelect from '~/components/ui/InputSelect'
+import InputRadioGroup from '~/components/ui/InputRadioGroup'
 import SubmitButton from '~/components/ui/SubmitButton'
 import FormSteps from '~/components/blocks/FormSteps'
+import FormSection from '~/components/ui/FormSection'
+import FieldGroup from '~/components/ui/FieldGroup'
 
 export default {
   components: {
     InputField,
+    InputSelect,
     SubmitButton,
-    FormSteps
+    FormSteps,
+    FormSection,
+    FieldGroup,
+    InputRadioGroup
   },
 
   middleware ({ store, redirect }) {
@@ -38,15 +134,53 @@ export default {
   data () {
     return {
       form: {
-        u_studies: ''
+        u_labor_code: '',
+        u_labor: '',
+        u_union: '',
+        u_studies: '',
+        u_movements: '',
+        u_public_employee: 0,
+        u_public_employee_cat: '',
+        u_public_employee_type: '',
+        sectorials: ''
       },
-      submitting: false
+      submitting: false,
+      isPublicEmployee: false,
+      publicEmployeeOptions: [
+        { value: 1, text: 'Administració de l’Estat' },
+        { value: 2, text: 'Administració autonòmica' },
+        { value: 3, text: 'Administració provincial' },
+        { value: 4, text: 'Administració local' },
+        { value: 5, text: 'Sanitat' },
+        { value: 6, text: 'Educació' },
+        { value: 7, text: 'Justícia' },
+        { value: 100, text: 'Altre' }
+      ],
+      publicEmployeeCategory: [
+        { value: 'A1', text: 'A1' },
+        { value: 'A2', text: 'A2' },
+        { value: 'B', text: 'B' },
+        { value: 'C1', text: 'C1' },
+        { value: 'C2', text: 'C2' },
+        { value: 'APF', text: 'APF' }
+      ],
+      publicEmployeeType: [
+        { value: 'C', text: 'C' },
+        { value: 'I', text: 'I' }
+      ]
     }
   },
 
   computed: {
     errors () {
       return this.$store.state.errors
+    },
+    laborCodes () {
+      return [
+        { value: 0, text: 'Code 0' },
+        { value: 1, text: 'Code 1' }
+      ]
+      // todo: map through this.$store.info.labor_codes, return [{value: obj-name, text: obj-value}]
     }
   },
 
@@ -87,5 +221,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.fade-enter-active, .fade-leave-active {
+  transition: .25s ease-in-out;
+  max-height: 1000px;
+}
+.fade-enter, .fade-leave-to {
+  max-height: 0;
+  transform: translateY(-1.5rem);
+  opacity: 0;
+}
 </style>
